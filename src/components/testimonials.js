6bbcf2b0d264241b1e152892daeb07894d2652e0
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-06-17 17:57:05 +0200

    created first testiomony initial structure

---
+++ src/components/testimonials.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Testimony(){
+  return (
+    <div className="testimony-container">
+      <img
+        className='testimony-img'
+        src={require('../images/img1.png')}
+        alt='First testimony person' />
+    </div>
+  )
+}
