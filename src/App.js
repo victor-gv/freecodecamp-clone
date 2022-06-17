112d343a5d17204d53a9f402a2f56c3592b946ab
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-06-17 18:24:40 +0200

    added first testimony

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimony from './components/testimonials.js';
 
 function App() {
   return (
     <div className="App">
-     
+     <Testimony />
     </div>
   );
 }
--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -4,9 +4,17 @@ function Testimony(){
   return (
     <div className="testimony-container">
       <img
-        className='testimony-img'
+        className='testimony-container__img'
         src={require('../images/img1.png')}
         alt='First testimony person' />
+
+     <div className="testimony-container__text">
+        <p className='testimony-name'>Emma Bostian in Sweden</p>
+        <p className='testimony-job'>Software Engineer at Spotify</p>
+        <p className='testimony-text'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+      </div>
     </div>
   )
 }
+
+export default Testimony;
